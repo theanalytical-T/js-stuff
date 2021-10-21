d979da7f15f14057eaa9925904463946eeb35d67
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-21 13:55:33 -0500

    10/21/2021
    Learning about innerHTML and adding to the DOM multiple elements.

--- Learning Javascript/dist/app.js
+++ Learning Javascript/dist/app.js
@@ -41,3 +41,86 @@ if (age < 6) {
 } else {
     console.log("senior citizen discount")
 }
+
+//Practicing for loops
+// let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
+// console.log("The 5 largest countries in the world:")
+// for (let i = 0; i < largeCountries.length; i++) {
+//     console.log("- " + largeCountries[i])
+// }
+
+//Practicing unshift, shift, pop, push
+let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"]
+
+// You need to help me fixup the largeCountries array so that 
+// China and Pakistan are added back into their respective places
+
+// Use push() & pop() and their counterparts unshift() & shift()
+// Google how to use unshift() and shift()
+
+largeCountries.shift()
+largeCountries.unshift("China")
+largeCountries.pop()
+largeCountries.push("Pakistan")
+
+console.log(largeCountries)
+
+//Practicing logical operators
+let dayOfMonth = 31
+let weekday = "Friday"
+
+if (dayOfMonth === 13 && weekday === "Friday") {
+    console.log("😱")
+}
+
+// Create a function that returns a random item from the array
+let hands = ["rock", "paper", "scissor"]
+
+function getHand() {
+    let randomIndex = Math.floor(Math.random() * hands.length)
+    return hands[randomIndex]
+}
+
+console.log(getHand())
+
+
+//Newer version of how to run a function when a button is clicked
+// let inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click", function () {
+//     console.log("Button clicked from addEventListener")
+// })
+
+//If possible, use const. If not, use let.
+const basePrice = 260
+const discount = 120
+let shippingCost = 12
+let shippingTime = "5-7 days"
+
+shippingCost = 15
+shippingTime = "7-14 days"
+
+let totalPurchase = basePrice + shippingCost / discount
+
+console.log("Total Estimate: $" + totalPurchase + ". Shipping in " + shippingTime)
+
+let mySaves = []
+
+for (let i = 0; i < mySaves.length; i++) {
+    //     //The += allows the loop to take the previous value and concatenate it with the current array the user is looking at
+    ulEl.innerHTML += "<li>" + mySaves[i] + "</li>"
+}
+
+// How to use .innerHTML to render a Buy! button inside a div container
+const container = document.getElementById("container")
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+my first solution-- > container.innerHTML = "<button>" "Buy!" "</button>"
+
+// When clicked, render a paragraph under the button (in the container)
+// that says "Thank you for buying!"
+
+function buy() {
+    // Use += because this signifies it ADDING to the DOM instead of replacing the previous element
+    container.innerHTML += "<p>Thank you for buying!</p>"
+}
+
