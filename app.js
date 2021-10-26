16ffb08cbba4c625c8c43140ec6704db932ccff9
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-26 14:21:51 -0500

    10/26/2021
    Reiterating how to pull from localStorage.

--- app.js
+++ app.js
@@ -159,3 +159,22 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+// truthy
+// falsy
+
+//These are falsy values
+// false
+// 0
+// ""
+// null --> How the developer signalizes emptiness
+// undefined --> How JavaScript signalizes emptiness
+// NaN
+
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+// localStorage.setItem("myCredits", "100")
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
